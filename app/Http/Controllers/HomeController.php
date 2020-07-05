a87812cadf5e1ab52a50e33424c1b9bea2f8ab8b
zero4897hn <zero4897hncool@gmail.com>
2020-07-05 13:40:29 +0700

    removing constructor in HomeCOntroller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,11 +7,6 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    // public function __construct()
-    // {
-    //     $this->middleware('auth');
-    // }
-
     /**
      * Display a listing of the resource.
      *
