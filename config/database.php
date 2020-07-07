b567a470604849c8926e1eea0b1303a9aa3cc0ae
zero4897hn <zero4897hncool@gmail.com>
2020-07-07 22:08:48 +0700

    Re-config database

--- config/database.php
+++ config/database.php
@@ -21,8 +21,8 @@ return [
     |
     */
 
-    // 'default' => env('DB_CONNECTION', 'pgsql_production'),
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql_production'),
+    // 'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
