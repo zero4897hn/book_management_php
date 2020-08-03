a36b2bb78bdd05cd51f6aefaaa6b533f6b31d6a0
zero4897hn <zero4897hncool@gmail.com>
2020-08-03 11:11:39 +0700

    Fixed Blocked loading mixed active content bug

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,11 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css">
     <title>Quản lý sách</title>
 </head>
 <body>
     <div id="root"></div>
-    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ asset('js/app.js') }}" type="text/javascript"></script>
 </body>
 </html>
